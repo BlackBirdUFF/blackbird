bd7021a06b17b3390296436eec9f44a6b5009585
Sergio Herman <sergioherman@id.uff.br>
2023-08-06 00:14:14 -0300

    Versão para Desktop do Menu da Header

---
+++ assets/js/header.js
@@ -0,0 +1,59 @@
+function showMenu1() {
+    var expandedMenu = document.getElementById("menu-susp1");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu1() {
+    var expandedMenu = document.getElementById("menu-susp1");
+    expandedMenu.style.display = "none";
+}
+
+function showMenu2() {
+    var expandedMenu = document.getElementById("menu-susp2");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu2() {
+    var expandedMenu = document.getElementById("menu-susp2");
+    expandedMenu.style.display = "none";
+}
+
+function showMenu3() {
+    var expandedMenu = document.getElementById("menu-susp3");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu3() {
+    var expandedMenu = document.getElementById("menu-susp3");
+    expandedMenu.style.display = "none";
+}
+
+function showMenu4() {
+    var expandedMenu = document.getElementById("menu-susp4");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu4() {
+    var expandedMenu = document.getElementById("menu-susp4");
+    expandedMenu.style.display = "none";
+}
+
+function showMenu5() {
+    var expandedMenu = document.getElementById("menu-susp5");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu5() {
+    var expandedMenu = document.getElementById("menu-susp5");
+    expandedMenu.style.display = "none";
+}
+
+function showMenu6() {
+    var expandedMenu = document.getElementById("menu-susp6");
+    expandedMenu.style.display = "inherit";
+}
+
+function hideMenu6() {
+    var expandedMenu = document.getElementById("menu-susp6");
+    expandedMenu.style.display = "none";
+}
---
+++ exemplo/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var expandedMenu = document.getElementById("expandedMenu");
+  if (expandedMenu.style.display === "none") {
+    expandedMenu.style.display = "block";
+  } else {
+    expandedMenu.style.display = "none";
+  }
+}
